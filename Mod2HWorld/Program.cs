6527f88d7b7aaf999ab71a5a0e555fd45291db74
Michael Falstrup <michael.falstrup@qiteq.dk>
2017-11-01 13:58:38 +0100

    Opgave Hello World modul 2

---
+++ Mod2HWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2HWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String navn = Console.ReadLine();
+            Console.WriteLine("Velkommen " + navn.ToUpper());
+            Console.ReadKey();
+        }
+    }
+}
