c2358505d49d57f8eb614022519b89b61bcd8eab
Michael Falstrup <michael.falstrup@qiteq.dk>
2017-11-02 10:05:14 +0100

    changes

---
+++ Mod3Variabler/Program.cs
@@ -0,0 +1,72 @@
+﻿/// <summary>
+/// ædlgdælfgk
+/// </summary>
+namespace Mod3Variabler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// sdfsdf
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// dsffsf
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            int htal = 10;
+            Console.WriteLine(htal);
+            htal++;
+            Console.WriteLine(htal);
+            htal--;
+            Console.WriteLine(htal);
+            htal += 20;
+            Console.WriteLine(htal);
+
+            double kommatal = 12.5;
+            Console.WriteLine(kommatal);
+            kommatal++;
+            Console.WriteLine(kommatal);
+            kommatal--;
+            Console.WriteLine(kommatal);
+            kommatal *= 2;
+            Console.WriteLine(kommatal);
+
+            DateTime dato = DateTime.Now;
+            Console.WriteLine(dato.ToString("ddMMyy"));
+            Console.WriteLine(dato.ToString("ddMMyyyy"));
+            Console.WriteLine(dato.ToString("dMy"));
+            Console.WriteLine(dato.ToLongDateString());
+
+            FilTyper ft = FilTyper.csv;
+            Console.WriteLine(ft);
+
+            Person p = new Person();
+            p.Id = 1;
+            p.Navn = "Mikkel";
+            Console.WriteLine(p.Navn + " " + p.Id);
+
+
+            Console.ReadKey();
+        }
+    }
+    enum FilTyper
+    {
+        csv,
+        pdf,
+        txt
+    };
+
+    struct Person
+    {
+        public int Id;
+        public string Navn;
+    }
+}
